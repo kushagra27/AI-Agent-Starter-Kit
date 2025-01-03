92ae927f654fe9e42c977d70f066b4f224435854
Gita Alekhya Paul <54375111+gitaalekhyapaul@users.noreply.github.com>
2025-01-03 14:53:36 +0530

    feat: add local lit actions development
    Signed-off-by: Gita Alekhya Paul <54375111+gitaalekhyapaul@users.noreply.github.com>

---
+++ lit-actions/src/actions/hello-action.ts
@@ -0,0 +1,25 @@
+// Add this to the top of the file, so that it can reference the global.d.ts file
+/// <reference path="../global.d.ts" />
+
+const go = async () => {
+  // Prints out the Lit Auth context
+  console.log("Lit.Auth:", Lit.Auth);
+  // Converts the public key to a token ID
+  const tokenId = await Lit.Actions.pubkeyToTokenId({ publicKey });
+  console.log("tokenId:", tokenId);
+  // Gets the permitted auth methods for the token ID
+  const permittedAuthMethods = await Lit.Actions.getPermittedAuthMethods({
+    tokenId,
+  });
+  console.log("permittedAuthMethods:", permittedAuthMethods);
+  // Signs the ECDSA signature
+  const signature = await Lit.Actions.signEcdsa({ publicKey, toSign, sigName });
+  // Sets the response to the Lit Actions context
+  Lit.Actions.setResponse({
+    response: JSON.stringify({ HelloName: helloName }),
+  });
+  // Returns the signature
+  return signature;
+};
+
+go();
